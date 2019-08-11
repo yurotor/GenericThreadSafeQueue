c96c97c69674ec1ad54d0b5a558552dca0ad4c2a
Uri Keselman <ukeselman@crossriverbank.com>
2019-08-11 17:35:06 +0300

    Add project files.

---
+++ GenericThreadSafeQueue/GenericQueue.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericThreadSafeQueue
+{
+    public class GenericQueue
+    {
+        ConcurrentDictionary<Type, ConcurrentQueue<object>> _items = new ConcurrentDictionary<Type, ConcurrentQueue<object>>();
+
+        public int Count => _items.Sum(i => i.Value.Count);
+
+        public GenericQueue()
+        {
+
+        }
+
+        public GenericQueue(IEnumerable<object> collection)
+        {
+            foreach (var item in collection)
+            {
+                Enqueue(item);
+            }
+        }
+
+        public void Enqueue<T>(T item)
+        {
+            if (_items.TryGetValue(typeof(T), out var q))
+            {
+                q.Enqueue(item);
+            }
+            else
+            {
+                _items.TryAdd(typeof(T), new ConcurrentQueue<object>(new object[] { item }));
+            }
+        }
+
+        public bool TryDequeue<T>(out T item)
+        {
+            item = default(T);
+            if (_items.TryGetValue(typeof(T), out var q))
+            {
+                if(q.TryDequeue(out var i))
+                {
+                    item = (T)i;
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+
+        public bool TryPeek<T>(out T item)
+        {
+            item = default(T);
+            if (_items.TryGetValue(typeof(T), out var q))
+            {
+                if (q.TryPeek(out var i))
+                {
+                    item = (T)i;
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+
+        public T Dequeue<T>()
+        {
+            if (_items.TryGetValue(typeof(T), out var q))
+            {
+                q.TryDequeue(out var item);
+                return (T)item;
+            }
+            throw new KeyNotFoundException($"Could not find key: {nameof(T)}");
+        }        
+        
+    }
+}
---
+++ GenericThreadSafeQueue/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericThreadSafeQueue
+{
+    class Program
+    {
+        static GenericQueue genericQueue = new GenericQueue();
+        static void Main(string[] args)
+        {
+            GetA();
+            GetB();
+
+            while (true)
+            {
+                Task.Delay(TimeSpan.FromSeconds(2)).Wait();
+                while (genericQueue.TryDequeue<A>(out var a))
+                {
+                    Console.WriteLine($"A {a.ID}");
+                }
+                while (genericQueue.TryDequeue<B>(out var b))
+                {
+                    Console.WriteLine($"B {b.ID}");
+                }
+               
+            }
+
+        }
+
+        public static void GetA()
+        {
+            Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    genericQueue.Enqueue(new A { ID = i });
+                    Task.Delay(TimeSpan.FromSeconds(1)).Wait();
+                }
+            });
+        }
+
+        public static void GetB()
+        {
+            Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    genericQueue.Enqueue(new B { ID = i });
+                    Task.Delay(TimeSpan.FromSeconds(1)).Wait();
+                }
+            });
+        }
+    }
+
+    class A
+    {
+        public int ID { get; set; }
+    }
+
+    class B
+    {
+        public int ID { get; set; }
+    }
+}
